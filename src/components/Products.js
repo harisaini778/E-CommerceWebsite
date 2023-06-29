d34011825ef5646590ad8f73e9fbba57c403105c
Hari Kumar Saini <harikumarsaini778@gmail.com>
2023-06-29 07:22:44 +0530

    Added product component in which added some dummy productsaand rendered on the screen

---
+++ src/components/Products.js
@@ -0,0 +1,42 @@
+import React from "react";
+
+
+const Products = () => {
+
+const productsArr = [
+{
+title: 'Colors',
+price: 100,
+imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%201.png',
+},
+
+{
+title: 'Black and white Colors',
+price: 50,
+imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%202.png',
+},
+
+{
+title: 'Yellow and Black Colors',
+price: 70,
+imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%203.png',
+},
+
+{
+title: 'Blue Color',
+price: 100,
+imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%204.png',
+}
+    ]
+
+    return (
+        <div>
+            {productsArr.map((items) => <ul>
+                <li>{items.title}</li>
+                <li>{items.price}</li>
+                <li>{items.imageUrl}</li>
+        </ul>)}
+        </div>
+    );
+}
+export default Products;
