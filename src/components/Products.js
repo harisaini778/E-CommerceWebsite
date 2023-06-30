8ade2f15a0f51761f960f10264fcc8429d332c97
Hari Kumar Saini <harikumarsaini778@gmail.com>
2023-06-30 22:45:08 +0530

    Modified the product component, because earlier it was rendering only one product in a single row, now by using for loop with step of 2, I have rendered two products in a single row, which makes the body more presentable, also I have updated style sheet of the product component

--- src/components/Products.js
+++ src/components/Products.js
@@ -1,43 +1,78 @@
 import React from "react";
-import { Row, Col } from "react-bootstrap";
+import { Row, Col, Button } from "react-bootstrap";
+import Container from "react-bootstrap/Container";
 import "./Products.css";
 
 const Products = () => {
   const productsArr = [
     {
-      title: 'Colors',
+      title: 'Album 1',
       price: 100,
       imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%201.png',
     },
     {
-      title: 'Black and white Colors',
+      title: 'Album 2',
       price: 50,
       imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%202.png',
     },
     {
-      title: 'Yellow and Black Colors',
+      title: 'Album 3',
       price: 70,
       imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%203.png',
     },
     {
-      title: 'Blue Color',
+      title: 'Album 4',
       price: 100,
       imageUrl: 'https://prasadyash2411.github.io/ecom-website/img/Album%204.png',
     }
   ];
 
-  return (
-    <div>
-      {productsArr.map((item, index) => (
-        <div className="product-container" key={index}>
+  const rows = [];
+
+  for (let i = 0; i < productsArr.length; i += 2) {
+    const firstProduct = productsArr[i];
+    const secondProduct = productsArr[i + 1];
+
+    const row = (
+      <Row key={i}>
+        <Col className="product-container">
+          <h2 className="product-title">{firstProduct.title}</h2>
+          <img src={firstProduct.imageUrl} alt={firstProduct.title} />
           <Row>
-                  <Col xs={6}>{item.title} {item.price}</Col>
-                   <Col xs={6}>
-              <img src={item.imageUrl} alt={item.title}/>
+            <Col>
+              <h5 className="product-price">Price: ${firstProduct.price}</h5>
+            </Col>
+            <Col>
+              <Button className="product-button">Add to cart</Button>
             </Col>
           </Row>
-        </div>
-      ))}
+        </Col>
+        {secondProduct && (
+          <Col className="product-container">
+            <h2 className="product-title">{secondProduct.title}</h2>
+            <img src={secondProduct.imageUrl} alt={secondProduct.title} />
+            <Row>
+              <Col>
+                <h5 className="product-price">Price: ${secondProduct.price}</h5>
+              </Col>
+              <Col>
+                <Button className="product-button">Add to cart</Button>
+              </Col>
+            </Row>
+          </Col>
+        )}
+      </Row>
+    );
+
+    rows.push(row);
+  }
+
+  return (
+    <div>
+      <Container>
+        <h1 className="product-heading">Music</h1>
+      </Container>
+      {rows}
     </div>
   );
 };
